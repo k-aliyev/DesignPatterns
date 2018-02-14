4bdfd75e0c6e5d96a35a796bfb666577afd45863
KamarnAl <Kamran.1234@yandex.ru>
2018-02-14 21:00:06 +0400

    Add Flyweight

---
+++ Flyweight/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    class FileManager
+    {
+        static Dictionary<int, Image> images = new Dictionary<int, Image>();
+
+        
+
+        static public Image GetImage(int i) {
+            Image image;
+            if (!images.ContainsKey(i))
+            {
+                switch (i)
+                {
+                    case 1:
+                        images.Add(1, Image.FromFile(@"One.png"));
+                        break;
+                    case 2:
+                        images.Add(2, Image.FromFile(@"Two.png"));
+                        break;
+                    case 3:
+                        images.Add(3, Image.FromFile(@"Three.png"));
+                        break;
+                    case 4:
+                        images.Add(4, Image.FromFile(@"Four.jpg"));
+                        break;
+                    case 5:
+                        images.Add(5, Image.FromFile(@"Five.jpg"));
+                        break;
+                }
+            }
+            image = images[i];
+            return image;
+        }
+        static public int GetCountOfDictionary()
+        {
+            return images.Count();
+        }
+    }
+}
---
+++ Flyweight/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flyweight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int[] NumImages = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                NumImages[i] = r.Next(1, 5);
+            }
+            pictureBox1.Image = FileManager.GetImage(NumImages[0]);
+            pictureBox2.Image = FileManager.GetImage(NumImages[1]);
+            pictureBox3.Image = FileManager.GetImage(NumImages[2]);
+            pictureBox4.Image = FileManager.GetImage(NumImages[3]);
+            MessageBox.Show(FileManager.GetCountOfDictionary().ToString());
+        }
+    }
+}
